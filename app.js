f465882b6aa65a8319980245dadcd4e7eb37d12f
gary <gary.white@lodgit.com>
2022-03-02 13:15:52 +0100

    code working

---
+++ app.js
@@ -0,0 +1,14 @@
+function xor(a, b) {
+    // TODO: Program Me
+    if (a != b){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(xor(false, false)); // false
+console.log(xor(true, false)); // true
+console.log(xor(false, true)); // true
+console.log(xor(true, true)); // False
+console.log(xor(true, true)); // true
